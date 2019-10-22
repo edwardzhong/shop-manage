2b7fb406035a4f6a1302669e68c912dcee8ad74f
edwardzhong <edwardloveyou@gmail.com>
2019-10-22 12:57:24 +0800

    update request

--- src/common/request.js
+++ src/common/request.js
@@ -1,28 +1,29 @@
 import axios from 'axios'
-import { stringify } from 'qs'
-import Storage from './storage'
-const storage = new Storage('account');
+import config from '../config/app'
 
-axios.defaults.baseURL = 'http://118.31.61.9:8181/index.php?_t='+Math.random()+'&r=';
+axios.defaults.baseURL = config.url;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 
 axios.interceptors.request.use(function(config) {
-    let data = config.method == "get" ? config.params || {} : config.data || {};
-    let account = storage.get();
-    if (account && account.token) {
-        Object.assign(data,{token:account.token});
+    const info = localStorage.getItem("selfInfo");
+    const params = config.method == "get" ? config.params || {} : config.data || {};
+    if (info) {
+        const obj = JSON.parse(info);
+        if (!params.userId && obj && obj.id) {
+            Object.assign(params, { userId: obj.id });
+        }
     }
-    config.data = stringify(data);
+    params._t = Math.random();
     return config;
 });
 
 axios.interceptors.response.use(function(res) {
     if (res.data.code == 200) {
         if (res.config.url.search(/\/login\/sign/i) > -1 && res.data.data) {
-            storage.save(res.data.data);
+            // storage.save(res.data.data);
         }
         if (res.config.url.search(/\/login\/signout/i) > -1) {
-            storage.clear();
+            // storage.clear();
         }
     }
     return res.data;
